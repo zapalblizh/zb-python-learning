8c0ca35bf330970fa4d0ab730ff14c2245889da3
Aleksandr Zapparov <zapal.blizh@gmail.com>
2022-01-14 14:05:26 +0100

    end 93

--- buy_computer.py
+++ buy_computer.py
@@ -1,3 +1,11 @@
+available_parts = ["computer",
+                   "monitor",
+                   "keyboard",
+                   "mouse",
+                   "mouse mat",
+                   "hdmi cable",
+                   "dvd drive"
+                   ]
 current_choice = "-"
 computer_parts = []  # create an empty list
 
@@ -18,13 +26,8 @@ while current_choice != '0':
             computer_parts.append("hdmi cable")
     else:
         print("Please add options to the list below:")
-        print("1. computer")
-        print("2. monitor")
-        print("3. keyboard")
-        print("4. mouse")
-        print("5. mouse mat")
-        print("6. hdmi cable")
-        print("0. to finish")
+        for part in available_parts:
+            print("{0}: {1}".format(available_parts.index(part) + 1, part))
 
     current_choice = input()
 
