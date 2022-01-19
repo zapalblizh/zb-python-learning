92fe947a0c7e28c027b608229b1526577a42428d
Aleksandr Zapparov <zapal.blizh@gmail.com>
2022-01-19 18:58:07 +0100

    Start of 95

--- buy_computer.py
+++ buy_computer.py
@@ -26,8 +26,8 @@ while current_choice != '0':
             computer_parts.append("hdmi cable")
     else:
         print("Please add options to the list below:")
-        for part in available_parts:
-            print("{0}: {1}".format(available_parts.index(part) + 1, part))
+        for number, part in enumerate(available_parts):
+            print("{0}: {1}".format(number + 1, part))
 
     current_choice = input()
 
