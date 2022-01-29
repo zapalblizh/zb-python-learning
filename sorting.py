84c66eeeb09cd37dff5ce026fb23f5977fb715f6
Aleksandr Zapparov <zapal.blizh@gmail.com>
2022-01-29 10:38:53 +0100

    start of 100

---
+++ sorting.py
@@ -0,0 +1,15 @@
+pangram = "The quick brown fox jumps over the lazy dog"
+
+letters = sorted(pangram)
+print(letters)
+
+numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+
+numbers.sort()
+print(numbers)
+
+missing_letter = sorted("The quick brown fox jumps over the lazy dog")
+print(missing_letter)
